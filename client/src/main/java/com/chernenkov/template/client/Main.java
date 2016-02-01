66b5dcd6089c35e6384fd7b2fd216f060aea28ac
doller <chernenkov.andrei@Yandex.ru>
2016-02-01 23:31:51 +0300

    basic

---
+++ client/src/main/java/com/chernenkov/template/client/Main.java
@@ -0,0 +1,27 @@
+package com.chernenkov.template.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.http.client.*;
+import com.google.gwt.user.client.ui.*;
+import com.vaadin.polymer.Polymer;
+import com.vaadin.polymer.elemental.Function;
+
+
+public class Main implements EntryPoint {
+
+
+    public void onModuleLoad() {
+        Polymer.importHref("iron-icons/iron-icons.html", new Function() {
+            public Object call(Object o) {
+                startApp();
+                return null;
+            }
+        });
+
+
+    }
+
+    public void startApp(){
+        RootPanel.get().add(new VaadinWidget());
+    }
+}
